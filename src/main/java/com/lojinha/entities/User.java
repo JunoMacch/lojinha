a47aa936d286cf7e907a6427f76228ec758354ca
Juno <jrmacch@outlook.com>
2025-05-22 03:26:23 -0300

    adicionando propriedades de teste e banco h2

--- src/main/java/com/lojinha/entities/User.java
+++ src/main/java/com/lojinha/entities/User.java
@@ -1,12 +1,17 @@
 package com.lojinha.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@Entity
+@Table(name = "tb_user") //renomear a tabela para tb_user pq a palavra User é uma palavra reservada do BD h2
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
