daa076639ceaf2abcce98707630a0b15939a4f99
Juno <jrmacch@outlook.com>
2025-05-23 01:35:13 -0300

    Adicionando camada de serviço e componentes

--- src/main/java/com/lojinha/repositories/UserRepository.java
+++ src/main/java/com/lojinha/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package com.lojinha.repositories;
 import com.lojinha.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Aqui a annotation @Repository se torna opcional pq a classe
+ * UserRepository ja esta herdando de JpaRepository que já esta registrada como componente do Spring
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
