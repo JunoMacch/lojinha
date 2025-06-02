f84d3b4ad057c4bb4e86e9ba10980f41dfb098d7
Juno <jrmacch@outlook.com>
2025-06-02 12:47:02 -0300

    adicionando enum e logica de int no enum

--- src/main/java/com/lojinha/config/TestConfig.java
+++ src/main/java/com/lojinha/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.lojinha.config;
 
 import com.lojinha.entities.Order;
 import com.lojinha.entities.User;
+import com.lojinha.entities.enums.OrderStatus;
 import com.lojinha.repositories.OrderRepository;
 import com.lojinha.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Junior", "jr@gmail.com", "911111111", "123");
         User user2 = new User(null, "Clara", "cla@gmail.com", "922222222", "123");
 
-        Order order1 = new Order(null, Instant.parse("2025-05-30T02:40:05Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2025-05-30T02:53:05Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2025-05-30T03:30:05Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2025-05-30T02:40:05Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2025-05-30T02:53:05Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2025-05-30T03:30:05Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
--- src/main/java/com/lojinha/entities/Order.java
+++ src/main/java/com/lojinha/entities/Order.java
@@ -1,6 +1,7 @@
 package com.lojinha.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.lojinha.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -26,6 +27,8 @@ public class Order implements Serializable {
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 
     /**
      * Esse é o lado muitos para um (*...1) Onde muitos pedidos podem ter 1 cliente
@@ -41,9 +44,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -61,6 +65,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
